44569cdb0f91a5a40ef6db60d347f48615bf6fcd
awyner <awyner.git@gmail.com>
2022-10-25 18:32:20 -0400

    Modified ClosestPair.java

--- CS-366/closest-pair/ClosestPair.java
+++ CS-366/closest-pair/ClosestPair.java
@@ -92,23 +92,59 @@ public class ClosestPair
 
 	public static double rec_cl_pair(Point[] p, int i, int j)
 	{
-		i=0;
-		j=2;
-		if ((j - i) < 3) 		 // at most 3 points in p[i..j]
+		int m;
+		double line;
+		double deltaL;
+		double deltaR;
+		double delta;
+		Point[] v = new Point[p.length];
+		
+		if ((j - i) < 3) 			 // at most 3 points in p[i..j]
 		{
-			double delta;
-			printPoints(p);
+
 			p = sort_by_y(p, i, j);  // re-order p[i..j] by y-coordinate
-			System.out.println("##########");
-			printPoints(p);
-			// delta = eucDistance(p[i], p[i+1]);
-			/* if (j - i == 1)      // there are only two points
-			return delta
-			else
-			return min(delta, distance(p[i+1], p[i+2]), distance(p[i], p[i+2])) */
+			delta = eucDistance(p[i], p[i+1]);
+
+			if (j - i == 1){      	 // there are only two points
+				return delta;
+			} else {
+				return Math.min(delta, Math.min(eucDistance(p[i+1], p[i+2]), eucDistance(p[i], p[i+2])));
+			}
 		}
-		   
-		return 0;
+		m = (i + j)/2;
+		line = p[m].x; 						// line = middle of p[i..j] by x-values
+		deltaL = rec_cl_pair(p, i, m);		// deltaL = min distance of left half
+		deltaR = rec_cl_pair(p, m+1, j); 	// deltaR = min distance of right half
+		delta = Math.min(deltaL, deltaR); 	// smallest distance of the two halves
+		p = merge_by_y(p, i, m, j); 		// merge so that p[i..j] is sorted by y
+		
+		// Of points in p[i..j], find points in vertical strip of width 2*delta,
+ 		// centered at line, store in temp array v, and t = number of pts
+		int t = 0;
+		for (int k=i; k<j; k++) 
+		{
+			if (p[k].x > line - delta && p[k].x < line + delta)
+			{
+				t = t + 1;
+				v[t] = p[k];
+			}
+	 	}
+		// Find closest pair among points in array v. NOTE: Cool math shows
+		// each point in the strip only needs to be compared to, at most,
+		// the next 7 other points. Any others are further away than delta.
+		for (int k = 0; k < t-1; k++)
+		{
+			for (int s = k+1; s < Math.min(t,k+7); s++) // inner loop iterates <= 7 times
+				delta = Math.min(delta, eucDistance(v[k],v[s]));
+		}
+
+		return delta;
+	}
+	
+	private static Point[] merge_by_y(Point[] p, int i, int m, int j)
+	{
+		
+		return p;
 	}
 
 	private static Point[] sort_by_y(Point[] p, int i, int j) 
@@ -138,6 +174,52 @@ public class ClosestPair
 		merge(p, left, middle, right);
 	}
 
+	public static void merge_by_y(Point p[], int i, int k, int j)
+	{
+		Point[] temp = new Point[j-i+1];
+
+		int c = 0;
+
+		int l = i;
+		int r = k+1;
+
+		while (l <= k && r <= j)
+		{
+			if (p[l].y <= p[r].y)
+			{
+				temp[c] = new Point(p[l].x, p[l].y);
+				c++;
+				l++;
+			}
+			else
+			{
+				temp[c] = new Point(p[r].x, p[r].y);
+				c++;
+				r++;
+			}
+		}
+		while (l <= k)
+		{
+			temp[c] = new Point(p[l].x, p[l].y);
+			c++;
+			l++;
+		}
+		while (r <= j)
+		{
+			temp[c] = new Point(p[r].x, p[r].y);
+			c++;
+			r++;
+		}
+
+		c = 0;
+		for (int t = i; t <= j; t++)
+		{
+			p[t].x = temp[c].x;
+			p[t].y = temp[c].y;
+			c++;
+		}
+	}
+	
 	public static void merge(Point[] p, int i, int k, int j)
 	{
 		Point[] temp = new Point[j-i+1];
