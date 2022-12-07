a439c02dc7c136964a07c06cdd07d3e77e404b26
awyner <alex.wyner2@gmail.com>
2022-12-07 00:12:05 -0500

    Start aoc/day7

---
+++ aoc/day7/day7.py
@@ -0,0 +1,10 @@
+def main():
+    with open("day7\\input.txt") as f:
+        filesystem = ['/']
+        # dir => [name]
+        # file = int size
+        # Make a class?
+        for line in f:
+          pass
+
+main()
