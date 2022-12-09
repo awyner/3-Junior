3e8e8e93f45ec52c2e3adb847fa6c71c563bc37a
awyner <alex.wyner2@gmail.com>
2022-12-09 00:32:15 -0500

    Start aoc/day9 what the fuck

---
+++ aoc/day9/day9.py
@@ -0,0 +1,29 @@
+WIDTH = 50
+
+def main():
+    x1, x2 = WIDTH-1, WIDTH-1
+    y1, y2 = 0, 0
+    head = grid[x1][y1]
+    tail = grid[x2][y2]
+    with open("day9\\input2.txt") as f:
+        for line in f:
+            line = line.strip().split()
+            if line[0] == 'R':
+                for x in range(int(line[1])):
+                    y1+=1
+                    head = grid[x1][y1]
+                    if y1 - y2 > 1:
+                        y2 += 1
+                        tail = grid[x2][y2]
+                        grid[x2][y2] = 1
+            elif line[0] == 'L':
+                for x in range(int(line[1])):
+                    y1-=1
+                    head = grid[x1][y1]
+                    if y2 - y1 > 1:
+                        y2 -= 1
+                        tail = grid[x2][y2]
+                        grid[x2][y2] = 1
+
+
+main()
