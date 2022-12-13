62b52fbcf2cfa7ff1a7cf5ec0185c97ad5bd0a6b
awyner <awyner.git@gmail.com>
2022-12-13 17:26:24 -0500

    Complete aoc/day13 pt 1

--- aoc/day13/day13.py
+++ aoc/day13/day13.py
@@ -14,34 +14,43 @@ def main():
                     index += 1
                     right = json.loads(line.strip())
                     count +=1
-                    if compare(left,right):
+                    result = compare(left,right)
+                    if result is None or result:
                         total += index
                         #print(total,index)
                     # print(left, right)
-                    #print(index, compare(left,right))
+                    print(index, total, compare(left,right))
                 case 2:
                     count = 0
     print(total)
 
 
 def compare(left, right):
-    current = True
+    current = None
     try:
+        if len(left) == 0 and len(right) != 0:
+                return True
         for x in range(len(left)):
             if isinstance(left[x], int) and isinstance(right[x], int):
-                return left[x] < right[x]:
+                if left[x] < right[x]:
+                    return True
+                elif left[x] > right[x]: 
+                    return False
+                
             elif isinstance(left[x], list) and isinstance(right[x], list):
                 current = compare(left[x],right[x])
+                if current is not None:
+                    return current
             else:
                 if isinstance(left[x], int):
                     l = [left[x]]
                     r = right[x]
-                else:
+                elif isinstance(right[x], int):
                     r = [right[x]]
                     l = left[x] 
                 current = compare(l,r)
-            if not current:
-                return current
+                if current is not None:
+                    return current
         return current                     
     except IndexError:
         return False
