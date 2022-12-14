49b4fd2e2677ae7fd1c3976fa2bb43fee6a3c683
awyner <awyner.git@gmail.com>
2022-12-14 17:19:32 -0500

    Complete aoc/day14

--- aoc/day14/day14.py
+++ aoc/day14/day14.py
@@ -4,23 +4,70 @@ dic = {}
 
 
 def main():
-    maxY = 0
-    with open("day14\\input.txt") as f:
+    floor = 0
+    with open("day14\\input2.txt") as f:
         for line in f:
             line = line.strip().split("->")
             for x in range(len(line)):
                 line[x] = line[x].strip().split(',')
                 line[x][0],line[x][1] = int(line[x][0]), int(line[x][1])
-                if line[x][0] > maxY:
-                    maxY = line[x][0]
-            drawRocks(line)
-        print(maxY*2)
-    dropSand()
-    # for x in range(SIDE):
-    #     for y in range(SIDE):
-    #         print(grid[x][y], end=' ')
-    #     print()
-    
+                if line[x][1] > floor:
+                    floor = line[x][1]
+            # drawRocks(line)
+            drawDic(line)
+    floor += 2
+    dropSandDic(floor)
+    # dropSand()
+    print('Floor:', floor)
+       
+
+def dropSandDic(floor):
+    count = 0
+    hitSource = False
+    row, col = 0, 500
+    while not hitSource:
+        if dic.get((0,500), '') != 'O':
+            if row >= floor:
+                dic[(row,col)] = 'O'
+                row,col = 0,500
+            if dic.get((row+1,col), '.') == '.':
+                row += 1
+            elif dic.get((row+1,col-1), '.') == '.':
+                row, col = row+1, col-1
+            elif dic.get((row+1,col+1), '.') == '.':
+                row, col = row+1, col+1
+            else:
+                dic[(row,col)] = 'O'
+                count+=1
+                row, col = 0, 500
+        else:
+            hitSource = True
+    print('Count dic', count)
+
+
+def drawDic(line):
+    for x in range(len(line)):
+        line[x][0] = line[x][0]  # - 494
+
+    for x in range(len(line)-1):
+        start = line[x]
+        end = line[x+1]
+        row1,col1,row2,col2 = start[1],start[0],end[1],end[0]
+        if row1 == row2:  # If x coords are same, increment through y
+            if col1 < col2:  # If start y is smaller than end y
+                for c in range(col1, col2+1):
+                    dic[(row1,c)] = '#'
+            if col1 > col2:
+                for c in range(col2, col1+1):
+                    dic[(row1,c)] = '#'
+        if col1 == col2:
+            if row1 < row2:
+                for r in range(row1, row2+1):
+                    dic[(r,col1)] = '#'
+            if row1 > row2:
+                for r in range(row2, row1+1):
+                    dic[(r,col1)] = '#'
+
 
 def dropSand():
     fellOff = False
@@ -28,7 +75,7 @@ def dropSand():
     while not fellOff:
         count += 1
         fellOff = rMoveSand(0,500)
-    print(count-1)
+    print('Count:', count-1)
 
 
 def rMoveSand(row, col):
