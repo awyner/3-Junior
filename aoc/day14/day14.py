34a800a3682444f268e9060c48b84429712cb67b
awyner <awyner.git@gmail.com>
2022-12-14 15:27:44 -0500

    Complete aoc/day14 pt1

--- aoc/day14/day14.py
+++ aoc/day14/day14.py
@@ -1,25 +1,71 @@
-SIDE = 10
+SIDE = 700
+grid = [['.'] * SIDE for x in range(SIDE)]
 
 
 def main():
-    with open("day14\\input2.txt") as f:
-        grid = ['.' * SIDE for x in range(SIDE)]
+    maxY = 0
+    with open("day14\\input.txt") as f:
         for line in f:
             line = line.strip().split("->")
             for x in range(len(line)):
                 line[x] = line[x].strip().split(',')
                 line[x][0],line[x][1] = int(line[x][0]), int(line[x][1])
-            grid = drawRocks(line, grid)
-            # print(grid)
+                if line[x][0] > maxY:
+                    maxY = line[x][0]
+            drawRocks(line)
+        print(maxY*2)
+    dropSand()
+    # for x in range(SIDE):
+    #     for y in range(SIDE):
+    #         print(grid[x][y], end=' ')
+    #     print()
+    
+
+def dropSand():
+    fellOff = False
+    count = 0
+    while not fellOff:
+        count += 1
+        fellOff = rMoveSand(0,500)
+    print(count-1)
 
 
-def drawRocks(line, grid):
+def rMoveSand(row, col):
+    try:
+        if grid[row+1][col] == '.':
+            return rMoveSand(row+1,col)
+        elif grid[row+1][col-1] == '.':
+            return rMoveSand(row+1,col-1)
+        elif grid[row+1][col+1] == '.':
+            return rMoveSand(row+1,col+1)
+        else:
+            grid[row][col] = 'O'
+    except IndexError:
+        return True
+                
+
+def drawRocks(line):
+    for x in range(len(line)):
+        line[x][0] = line[x][0]  # - 494
+
     for x in range(len(line)-1):
         start = line[x]
-        next = line[x+1]
-        print(start, next)
-    print()
-    return line
+        end = line[x+1]
+        row1,col1,row2,col2 = start[1],start[0],end[1],end[0]
+        if row1 == row2:  # If x coords are same, increment through y
+            if col1 < col2:  # If start y is smaller than end y
+                for c in range(col1, col2+1):
+                    grid[row1][c] = '#'
+            if col1 > col2:
+                for c in range(col2, col1+1):
+                    grid[row1][c] = '#'
+        if col1 == col2:
+            if row1 < row2:
+                for r in range(row1, row2+1):
+                    grid[r][col1] = '#'
+            if row1 > row2:
+                for r in range(row2, row1+1):
+                    grid[r][col1] = '#'
 
 
 main()
